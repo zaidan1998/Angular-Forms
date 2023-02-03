29eb930f61c9b533debf66d144b354f71635ec01
Ahmad Zaidan Adnan <ahmadzaidan.adnan@PETRONAS.COM>
2023-02-03 15:33:03 +0800

    New update

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -18,9 +20,21 @@ export class UserSettingsFormComponent implements OnInit {
 
   userSettings : UserSettings = { ...this.originalUserSettings };
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+  }
+
+  onBlur(field : NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
+    this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+      result => console.log('success: ', result),
+      error => console.log('error: ', error)
+    );
   }
 
 }
